a6f7c34fa11da49db5f79b1ecabd127b7cfea4bf
Yibo Jiang <yibo.jiang@coconutislandstudio.com>
2016-01-31 00:35:35 +0800

    add moving plat form

---
+++ Assets/Scripts/Moving.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moving : MonoBehaviour {
+	public Rigidbody2D body;
+	public Vector2 velocity;
+
+	void Awake(){
+		body=GetComponent<Rigidbody2D>();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		body.MovePosition(body.position+velocity*Time.fixedDeltaTime);
+	}
+}
