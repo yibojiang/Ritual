5b1eaa3e3506aac6dfd8e090cdc1a95a9e67033c
Yibo Jiang <yibo.jiang@coconutislandstudio.com>
2016-01-31 00:10:26 +0800

    add color generator

---
+++ Assets/Scripts/ColorGenerator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorGenerator : MonoBehaviour {
+	public Color color;
+	void OnTriggerEnter2D(Collider2D _collider){
+
+		if (_collider.CompareTag("Mob")){
+			Mob mob=_collider.GetComponent<Mob>();
+			
+			mob.AbsorbColor(color);
+				
+			
+		}
+	}
+}
